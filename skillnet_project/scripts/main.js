8706fee599fc799863ff65efb1b34871c6745f40
AZ70code <86321327+AZ70code@users.noreply.github.com>
2024-04-23 00:11:04 +0100

    lock body when mobile menu open

--- skillnet_project/scripts/main.js
+++ skillnet_project/scripts/main.js
@@ -4,8 +4,10 @@
 		document.addEventListener("click", function (e) {
 			if ( e.target.closest('.icon-menu')) {
 				document.documentElement.classList.toggle("menu-open");
+				document.body.classList.toggle('lock');
 			} else {
 				document.documentElement.classList.remove("menu-open");
+				document.body.classList.remove('lock')
 			}
 		});
 	};
