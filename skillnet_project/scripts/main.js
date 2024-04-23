e7e842573156fedbd3065896fad50732b98ce226
AZ70code <86321327+AZ70code@users.noreply.github.com>
2024-04-23 14:24:57 +0100

    add comments

--- skillnet_project/scripts/main.js
+++ skillnet_project/scripts/main.js
@@ -1,14 +1,17 @@
+'use strict'
 
-(function menuInit() {
-	if (document.querySelector(".icon-menu")) {
-		document.addEventListener("click", function (e) {
-			if ( e.target.closest('.icon-menu')) {
-				document.documentElement.classList.toggle("menu-open");
-				document.body.classList.toggle('lock');
-			} else {
-				document.documentElement.classList.remove("menu-open");
-				document.body.classList.remove('lock')
-			}
-		});
-	};
-})()
+window.onload = () => {
+	(function menuInit() {
+		if (document.querySelector(".icon-menu")) {
+			document.addEventListener("click", function (e) {
+				if (e.target.closest('.icon-menu')) {
+					document.documentElement.classList.toggle("menu-open");
+					document.body.classList.toggle('lock');
+				} else {
+					document.documentElement.classList.remove("menu-open");
+					document.body.classList.remove('lock')
+				}
+			});
+		};
+	})()
+};
