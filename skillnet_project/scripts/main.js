32328cb28fe4f20941aec92ede42508a1e8f98b6
AZ70code <86321327+AZ70code@users.noreply.github.com>
2024-04-23 20:41:45 +0100

    add slider

--- skillnet_project/scripts/main.js
+++ skillnet_project/scripts/main.js
@@ -1,6 +1,7 @@
 'use strict'
 
 window.onload = () => {
+	// init menu burger
 	(function menuInit() {
 		if (document.querySelector(".icon-menu")) {
 			document.addEventListener("click", function (e) {
@@ -14,4 +15,51 @@ window.onload = () => {
 			});
 		};
 	})()
+
+	// init swiper
+	const swiperThumb = new Swiper('.classes__slider-thumbs', {
+		// Optional parameters
+
+		spaeed: 800,
+		spaceBetween: 20,
+		slidesPerView: 7,
+		freeMode: true,
+		watchSlidesProgress: true,
+		grid: {
+			rows: 1,
+		},
+
+	});
+	const swiperMain = new Swiper('.classes__slider', {
+		// Optional parameters
+		spaeed: 800,
+		loop: true,
+		// effect: 'fade',
+		spaceBetween: 10,
+		effect: "coverflow",
+		grabCursor: true,
+		centeredSlides: true,
+		slidesPerView: 3,
+		coverflowEffect: {
+			rotate: 70,
+			stretch: 0,
+			depth: 100,
+			modifier: 1,
+			slideShadows: true,
+		},
+		// If we need pagination
+		pagination: {
+			el: '.swiper-pagination',
+		},
+
+		// Navigation arrows
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+
+		thumbs: {
+			swiper: swiperThumb,
+		},
+	});
 };
